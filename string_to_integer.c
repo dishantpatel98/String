6413da42f0576b6a04addc5c56da05e99f457ac2
Dishant Janakbhai Patel <39313255+dishantpatel98@users.noreply.github.com>
2020-02-07 11:53:48 +0530

    Update string_to_integer.c

--- string_to_integer.c
+++ string_to_integer.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 
-int stoi(char str[]){                      /* To convert string to number we take each number from the string and subtracts its asci value from the 
-					      asci value of '0' and we store that number in the integer and we keep doing it until we get null character
+int stoi(char str[]){                      /* To convert string to number we take each number from the string and 
+                                              subtracts its asci value from the 
+					      asci value of '0' and we store that number in the integer and 
+					      we keep doing it until we get null character */
+
+
 int i,n;
-n=0;                                                                               */
+n=0;                                                                               
 
 for(i=0;str[i]>'0'&& str[i]<'9';i++)
 n=10*n+ ( str[i]-'0');
